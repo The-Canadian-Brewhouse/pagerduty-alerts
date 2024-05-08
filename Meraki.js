9e8525e5d5bccd31c8a8d957d99b90b8e5084c91
Dylan Grove <Dylangrove@outlook.com>
2024-05-08 13:09:08 -0600

    Further cleanup

--- Meraki.js
+++ Meraki.js
@@ -2,23 +2,28 @@
 var body = PD.inputRequest.body;
 var emitEvent = true;
 
+
 // Set Alert Severity
-var severity = "warning";
 // critical
 // error
 // warning
 // info
 // unknown
-
-if(body.alertType == "Settings changed") {severity = "info";}
-if(body.alertType == "Motion detected") {severity = "info";}
-if(body.alertType == "Network usage alert") {severity = "warning";}
-if(body.alertType == "Client IP conflict detected") {severity = "warning";}
-if(body.alertType == "APs went down") {severity = "critical";}
+var severity = "warning";
+const alertSeverityMap = {
+  "Settings changed": "info",
+  "Motion detected": "info",
+  "Network usage alert": "warning",
+  "Client IP conflict detected": "warning",
+  "Cellular came up": "warning",
+  "appliances went down": "critical",
+  "switches went down": "critical",
+  "appliances came up": "critical",
+  "switches came up": "critical",
+  "APs went down": "critical"
+};
+let severity = alertSeverityMap[body.alertType];
 if(body.alertType == "Uplink status changed" && !body.alertData.uplink && body.networkName != "PLD-Seton") {severity = "warning";}
-if(body.alertType == "Cellular came up") {severity = "warning";}
-if(body.alertType == "appliances went down") {severity = "critical";}
-if(body.alertType == "switches went down") {severity = "critical";}
 
 
 // Clear irrelavent ip conflict alerts
@@ -44,6 +49,7 @@ var rogueApBlacklist = [
 if ((body.alertType == "Air Marshal - Rogue AP detected") && (rogueApBlacklist.includes(body.alertData.ssidName))) {emitEvent = false;}
 if ((body.alertType == "Air Marshal - Rogue AP detected") && (body.alertData.ssidName.includes("Meraki"))) {emitEvent = false;}
 if ((body.alertType == "Air Marshal - Rogue AP detected") && (body.alertData.ssidName.includes("roku"))) {emitEvent = false;}
+
 
 // clear irrelavent dhcp alerts
 if ((body.alertType == "New DHCP server detected") && (body.alertData.ip.includes(".126"))) {emitEvent = false;}
